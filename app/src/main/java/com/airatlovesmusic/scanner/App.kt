f0904d469b3294e41212626b581cdc390c444a95
airatlovesmusic <airat@rcsmail.ru>
2020-09-17 18:44:47 +0300

    Init android app

---
+++ app/src/main/java/com/airatlovesmusic/scanner/App.kt
@@ -0,0 +1,9 @@
+package com.airatlovesmusic.scanner
+
+import android.app.Application
+
+/**
+ * Created by Airat Khalilov on 17/09/2020.
+ */
+
+class App: Application()
---
+++ app/src/main/java/com/airatlovesmusic/scanner/AppActivity.kt
@@ -0,0 +1,9 @@
+package com.airatlovesmusic.scanner
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Airat Khalilov on 17/09/2020.
+ */
+
+class AppActivity: AppCompatActivity()
